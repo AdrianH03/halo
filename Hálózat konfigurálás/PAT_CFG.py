f9042547eabaaea319c9d5a8ffede49bb5109929
TheKornelM <makari.kornel@gmail.com>
2023-04-24 11:49:06 +0200

    Hálózatkonfiguráció módosítás
    - A konfiguráció különböző lépései függvényekre lettek bontva
    - A routerek adatai egy tömbben találhatóak a könnyebb kezelés és bővíthetőség érdekében
    - Hibakezelés a csatlakozásnál
    - A lépések követhetőek kimeneti üzenetben

--- Hálózat konfigurálás/PAT_CFG.py
+++ Hálózat konfigurálás/PAT_CFG.py
@@ -1,93 +1,89 @@
 from netmiko import ConnectHandler
-
-# SZHELY1 belépési adatok
-router_szhely1 = {
-    'device_type': 'cisco_ios',
-    'ip': '172.16.100.241',
-    'username': 'your_username',
-    'password': 'your_password',
-}
-
-# SZHELY2 belépési adatok
-router_szhely2 = {
-    'device_type': 'cisco_ios',
-    'ip': '172.16.100.242',
-    'username': 'your_username',
-    'password': 'your_password',
-}
-
-# Csatlakozás a routerekhez
-szhely1 = ConnectHandler(**router_szhely1)
-szhely2 = ConnectHandler(**router_szhely2)
-
-# Belső ACL konfigurálása
-acl_belso = '''
-ip access-list standard PAT-inside
- permit 172.16.100.0 0.0.0.127
- permit 172.16.100.128 0.0.0.63
- permit 172.16.100.240 0.0.0.15
-'''
-# Belső ACL parancsok mentése
-szhely1.send_config_set(acl_belso.splitlines())
-szhely1.send_command('write memory')
-szhely2.send_config_set(acl_belso.splitlines())
-szhely2.send_command('write memory')
-
-# NAT inside, overload beállítása
-# NAT outside interface-t majd be kell állítani a megfelelőre
-nat_beallit = '''
-interface GigabitEthernet0/0/0
-ip nat outside
-exit
-interface GigabitEthernet0/2.100
-ip nat inside
-exit
-interface GigabitEthernet0/2.101
-ip nat inside
-exit
-interface GigabitEthernet0/2.102
-ip nat inside
-exit
-ip nat inside source list pat-inside interface GigabitEthernet0/0/0 overload
-exit
-'''
-szhely1.send_config_set(nat_beallit.splitlines())
-szhely1.send_command('write memory')
-szhely2.send_config_set(nat_beallit.splitlines())
-szhely2.send_command('write memory')
-
-# Configure ACLs on Router1 
-# GPT által generált, törölhető majd
-acl_inside_cmd_router1 = 'access-list acl-inside permit ip source-ip source-wildcard'
-acl_inside_router1 = 'access-list acl-inside permit ip 172.16.100.0 0.0.0.127 any'
-acl_outside_cmd_router1 = 'access-list acl-outside permit ip source-ip source-wildcard'
-acl_outside_router1 = 'access-list acl-outside permit ip any any'
-acl_inside_cmd_router1_vlan101 = 'access-list acl-inside permit ip source-ip source-wildcard'
-acl_inside_router1_vlan101 = 'access-list acl-inside permit ip 172.16.100.240 0.0.0.15 any'
-acl_inside_cmd_router1_vlan102 = 'access-list acl-inside permit ip source-ip source-wildcard'
-acl_inside_router1_vlan102 = 'access-list acl-inside permit ip 172.16.100.128 0.0.0.63 any'
-szhely1.send_config_set([acl_inside_cmd_router1, acl_inside_router1,
-                                     acl_outside_cmd_router1, acl_outside_router1,
-                                     acl_inside_cmd_router1_vlan101, acl_inside_router1_vlan101,
-                                     acl_inside_cmd_router1_vlan102, acl_inside_router1_vlan102])
-
-# Configure ACLs on Router2
-# GPT által generált, törölhető majd
-
-acl_inside_cmd_router2 = 'access-list acl-inside permit ip source-ip source-wildcard'
-acl_inside_router2 = 'access-list acl-inside permit ip 172.16.100.0 0.0.0.127 any'
-acl_outside_cmd_router2 = 'access-list acl-outside permit ip source-ip source-wildcard'
-acl_outside_router2 = 'access-list acl-outside permit ip any any'
-acl_inside_cmd_router2_vlan101 = 'access-list acl-inside permit ip source-ip source-wildcard'
-acl_inside_router2_vlan101 = 'access-list acl-inside permit ip 172.16.100.240 0.0.0.15 any'
-acl_inside_cmd_router2_vlan102 = 'access-list acl-inside permit ip source-ip source-wildcard'
-acl_inside_router2_vlan102 = 'access-list acl-inside permit ip 172.16.100.128 0.0.0.63 any'
-szhely2.send_config_set([acl_inside_cmd_router2, acl_inside_router2,
-                                     acl_outside_cmd_router2, acl_outside_router2,
-                                     acl_inside_cmd_router2_vlan101, acl_inside_router2_vlan101,
-                                     acl_inside_cmd_router2_vlan102, acl_inside_router2_vlan102])
-
-# NAT konfigurálás - lehet törölhető
-nat_konfig = 'ip nat inside source list acl-inside interface GigabitEthernet0/1 overload'
-szhely1.send_command(nat_konfig)
-szhely2.send_command(nat_konfig) 
+#
+# --- Router adatok
+# 
+routerek_adatok = [{
+    'device_type': 'cisco_ios_telnet',
+    'ip': '192.168.51.121',
+    'username': 'admin',
+    'password': 'admin',
+    'port': 23,
+    },{
+    'device_type': 'cisco_ios_telnet',
+    'ip': '192.168.51.122',
+    'username': 'admin',
+    'password': 'admin',
+    'port': 23,  
+}]
+routerek = []
+ip = []
+#
+# --- Csatlakozás a routerekhez
+#
+def csatlakoz(): 
+    n = 0
+    try:
+        for eszkoz in routerek_adatok:
+            print(f"\n{eszkoz['ip']}: Csatlakozás folyamatban...")
+            routerek.append(ConnectHandler(**eszkoz))
+            ip.append(eszkoz['ip'])
+            print(f"{eszkoz['ip']}: Sikeres csatlakozás")
+            acl_beallit(n)
+            n += 1
+    except Exception as e:
+        print("Hiba történt:\n"+str(e))
+#
+# --- ACL létrehozása a PAT-hoz
+#
+def acl_beallit(n):
+    print(f"{ip[n]}: Belső ACL konfigurálása folyamatban...")
+    # Belső ACL konfigurálás
+    acl_belso = '''
+    ip access-list standard PAT-inside
+    permit 172.16.100.0 0.0.0.127
+    permit 172.16.100.128 0.0.0.63
+    permit 172.16.100.240 0.0.0.15
+    '''
+    # Belső ACL parancsok mentése
+    routerek[n].send_config_set(acl_belso.splitlines())
+    print(f"{ip[n]}: PAT ACL konfiguráció sikeres")
+    nat_beallit(n)
+#
+# --- NAT beállítása az interfészeken
+#    
+def nat_beallit(n):
+    print(f"{ip[n]}: NAT beállítása folyamatban az interfészeken...")
+    nat_beallit = '''
+    interface GigabitEthernet0/0
+    no sh
+    ip nat outside
+    interface GigabitEthernet2/0
+    no sh
+    ip nat inside
+    interface GigabitEthernet2/0.100
+    no sh
+    ip nat inside
+    interface GigabitEthernet2/0.101
+    no sh
+    ip nat inside
+    interface GigabitEthernet2/0.102
+    no sh
+    ip nat inside
+    ip nat inside source list PAT-inside interface GigabitEthernet0/0 overload
+    '''
+    routerek[n].send_config_set(nat_beallit.splitlines())
+    print(f"{ip[n]}: NAT beállítva")
+    ment(n)
+#
+# --- A konfigfájlok mentése, illetve a kapcsolatok megszakítása
+#    
+def ment(n):
+        print(f"{ip[n]}: Konfiguráció mentése folyamatban...")
+        routerek[n].send_command('do write memory')
+        print(f"{ip[n]}: Változtatások sikeresen mentve a konfigfájlba")
+        routerek[n].disconnect()    
+        print(f"{ip[n]}: Kapcsolat vége")
+# --- Alapprogram
+csatlakoz()
+print("\nVáltoztatások elmentve a konfigfájlokba az összes routeren, kapcsolatok megszakítva")
+print("Script sikeresen lefutott")
